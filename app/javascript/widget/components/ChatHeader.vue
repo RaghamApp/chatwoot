48d8ef5f4f7a2eaa40c251369ed8ec5e740e600d
Erfan Safari <erfanshield@outlook.com>
2026-04-21 19:53:43 +0330

    update widget ChatHeader.vue

--- app/javascript/widget/components/ChatHeader.vue
+++ app/javascript/widget/components/ChatHeader.vue
@@ -27,13 +27,6 @@ const onBackButtonClick = () => {
 <template>
   <header class="flex justify-between w-full p-5 bg-n-background gap-2">
     <div class="flex items-center">
-      <button
-        v-if="showBackButton"
-        class="px-2 ltr:-ml-3 rtl:-mr-3"
-        @click="onBackButtonClick"
-      >
-        <FluentIcon icon="chevron-left" size="24" class="text-n-slate-12" />
-      </button>
       <img
         v-if="avatarUrl"
         class="w-8 h-8 ltr:mr-3 rtl:ml-3 rounded-full"
@@ -50,12 +43,6 @@ const onBackButtonClick = () => {
               ${isOnline ? 'bg-n-teal-10' : 'hidden'}`"
           />
         </div>
-        <AvailabilityContainer
-          :agents="availableAgents"
-          :show-header="false"
-          :show-avatars="false"
-          text-classes="text-xs leading-3"
-        />
       </div>
     </div>
     <HeaderActions :show-popout-button="showPopoutButton" />
